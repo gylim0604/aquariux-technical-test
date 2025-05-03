8dd2c0ecc6b8c5d94fd8c99a927473e4416258c7
gylim0604 <gylim0604@hotmail.com>
2025-05-03 13:47:21 +0800

    add typing

--- src/features/weatherSummary/weatherSummary.ts
+++ src/features/weatherSummary/weatherSummary.ts
@@ -1,6 +1,8 @@
+import { WeatherData } from './types';
+
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-export async function getCurrentWeather(params: { city: string; countryCode: string }): Promise<any> {
+export async function getCurrentWeather(params: { city: string; countryCode: string }): Promise<WeatherData> {
 	const { city, countryCode } = params;
 	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`);
 	if (!res.ok) throw new Error('Failed to fetch current weather');

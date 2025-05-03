9ba878c20fd70db6ba3ff451ad36ac834924e16c
gylim0604 <gylim0604@hotmail.com>
2025-05-03 13:17:09 +0800

    add api call for current weather

---
+++ src/features/weatherSummary/weatherSummary.ts
@@ -0,0 +1,8 @@
+const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+export async function getCurrentWeather(params: { city: string; countryCode: string }): Promise<any> {
+	const { city, countryCode } = params;
+	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`);
+	if (!res.ok) throw new Error('Failed to fetch current weather');
+	return res.json();
+}

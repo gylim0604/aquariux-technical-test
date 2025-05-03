e6e2cf0802674e78ac548d2a85b41d571214d462
gylim0604 <gylim0604@hotmail.com>
2025-05-04 00:14:30 +0800

    add weather forecast hook and typing

---
+++ src/features/weatherForecast/useWeatherForecast.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { getWeatherForecast } from './weatherForecast';
+import { Location } from '../weatherSummary/types';
+import { ForecastData, GroupedForecast, List } from './types';
+
+function processForecastData(data: ForecastData): any {
+	const grouped = data.list.reduce((prev: any, curr: List) => {
+		const date = curr.dt_txt.split(' ')[0];
+		if (!prev[date]) prev[date] = [];
+		prev[date].push(curr);
+		return prev;
+	}, {} as Record<string, List[]>);
+
+	return Object.entries(grouped).map(([date, entries]) => ({
+		date,
+		entries,
+	}));
+}
+
+export function useWeatherForecast(params: Location) {
+	const [data, setData] = useState<GroupedForecast[] | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const res = await getWeatherForecast(params);
+				const data = processForecastData(res);
+				setData(data);
+			} catch (err: any) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		}
+		fetchData();
+	}, [params]);
+
+	return { data, loading, error };
+}

b9965c63a08a3e3d8a39949f518412929c74714a
gylim0604 <gylim0604@hotmail.com>
2025-05-04 11:37:42 +0800

    add reusable layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<Home />}></Route>
+			<Route path='/' element={<MainLayout />}>
+				<Route index element={<Home />}></Route>
+			</Route>
 		</Routes>
 	);
 }
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+// src/layouts/LocationLayout.tsx
+import { Outlet } from 'react-router-dom';
+import { LocationBar } from '../features/locationBar/LocationBar';
+
+export default function MainLayout() {
+	return (
+		<div className='bg-slate-200  min-h-screen '>
+			<div className='bg-white py-2 flex flex-col items-center mb-4 top-0 sticky shadow-sm'>
+				<LocationBar />
+			</div>
+			<div className='flex flex-col items-center'>
+				<Outlet />
+			</div>
+		</div>
+	);
+}
